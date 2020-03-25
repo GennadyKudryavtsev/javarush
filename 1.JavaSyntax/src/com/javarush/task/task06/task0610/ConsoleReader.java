59d9c7760fb3fe2022204553c2833734e13494ec
GennadyKudryavtsev <gennady1504@yandex.ru>
2020-03-25 13:52:24 +0400

    Added task0609

--- 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -8,26 +8,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ConsoleReader {
-//    public static String readString() throws Exception {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String q = bufferedReader.readLine();
-//
-//    }
-//
-//    public static int readInt() throws Exception {
-//        int a = Integer.parseInt();
+    public static String readString() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String q = bufferedReader.readLine();
+        return q;
+    }
 
-//    }
+    public static int readInt() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String w = bufferedReader.readLine();
+        int a = Integer.parseInt(w);
+        return a;
+    }
 
-//    public static double readDouble() throws Exception {
-//        //напишите тут ваш код
-//
-//    }
-//
-//    public static boolean readBoolean() throws Exception {
-//        //напишите тут ваш код
-//
-//    }
+    public static double readDouble() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String r = bufferedReader.readLine();
+        double s = Double.parseDouble(r);
+        return s;
+    }
+
+    public static boolean readBoolean() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String e = bufferedReader.readLine();
+        boolean b = Boolean.parseBoolean(e);
+        return b;
+
+
+    }
 
     public static void main(String[] args) throws Exception {
 

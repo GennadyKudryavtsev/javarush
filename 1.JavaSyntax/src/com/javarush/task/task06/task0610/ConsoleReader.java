a7bcd2749b9eaf314e9730b0a9c57fcd4259b5f8
GennadyKudryavtsev <gennady1504@yandex.ru>
2020-03-25 12:47:33 +0400

    test

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task06.task0610;
+
+/* 
+Класс ConsoleReader
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ConsoleReader {
+//    public static String readString() throws Exception {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        String q = bufferedReader.readLine();
+//
+//    }
+//
+//    public static int readInt() throws Exception {
+//        int a = Integer.parseInt();
+
+//    }
+
+//    public static double readDouble() throws Exception {
+//        //напишите тут ваш код
+//
+//    }
+//
+//    public static boolean readBoolean() throws Exception {
+//        //напишите тут ваш код
+//
+//    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
